473bcbdabb9f87e64bb8fc94b0590b952149192e
Polaris✨ <108342258+Polariswright@users.noreply.github.com>
2023-05-30 12:07:40 -0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './paginas/nav/nav.component';
 import { FooterComponent } from './paginas/footer/footer.component';
 import { SectionComponent } from './paginas/section/section.component';
+import { PrincipalComponent } from './pag/principal/principal.component';
+import { RpgComponent } from './pag/rpg/rpg.component';
+import { PersonagensComponent } from './pag/personagens/personagens.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    SectionComponent
+    SectionComponent,
+    PrincipalComponent,
+    RpgComponent,
+    PersonagensComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pag/rpg/rpg.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rpg',
+  templateUrl: './rpg.component.html',
+  styleUrls: ['./rpg.component.css']
+})
+export class RpgComponent {
+
+}
